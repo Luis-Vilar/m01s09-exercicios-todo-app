58d4cc8b2387d53c0e6494f861fbd003c635afbd
Luis Federico Vilar Velazquez <124309725+Luis-Vilar@users.noreply.github.com>
2023-03-29 03:49:13 -0300

    Update Avatar.jsx

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,15 +1,23 @@
-export class Avatar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
-  render() {
-    return (
-      <img
-        src={this.props.src}
-        className="img-thumbnail"
-        alt={this.props.alt}
-      />
-    );
-  }
-}
+
+
+
+import React from "react"; 
+  
+ function Avatar (props) { 
+   return ( 
+     <img src={props.src} className="img-thumbnail" alt={props.alt} /> 
+   ); 
+ }; 
+  
+ export default Avatar;
+
+
+
+
+
+
+
+
+
+

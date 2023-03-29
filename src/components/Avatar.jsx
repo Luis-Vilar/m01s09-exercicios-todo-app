873ee67f8d2cdb27c7bb65af71d2f7eb7df6e7d9
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-03-29 11:17:47 -0300

    cross problem not solved

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,12 +1,12 @@
-import React from "react"; 
-  
- function Avatar (props) { 
-   return ( 
-     <img src={props.src} className="img-thumbnail" alt={props.alt} /> 
-   ); 
- }; 
-  
- export default Avatar;
+import React from "react";
+
+function Avatar (props) {
+  return (
+    <img src={props.src} className="img-thumbnail" alt={props.alt} />
+  );
+};
+
+export default Avatar;
 
 
 

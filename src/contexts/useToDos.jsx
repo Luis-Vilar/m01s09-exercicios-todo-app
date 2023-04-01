f853e942a977051871e654d24d94c853d3f7134e
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-03-31 21:33:25 -0300

    mejoras en useTodos

--- src/contexts/useToDos.jsx
+++ src/contexts/useToDos.jsx
@@ -1,7 +1,11 @@
-import { useContext } from 'react';
-import { todosContext } from './ToDoProvider';
+import { useContext } from "react";
+import { todosContext } from "./ToDoProvider";
 
 export const useToDos = () => {
-    const context = useContext(todosContext);  
+  const context = useContext(todosContext);
+  if (context) {
     return context;
-  };
+  } else {
+    return alert("useToDos deb ser usado dentro de  ToDoProvider");
+  }
+};
